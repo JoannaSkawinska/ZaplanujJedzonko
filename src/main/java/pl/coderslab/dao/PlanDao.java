9796c0c487236914cfe142f9558973fe9bed5ee1
Michal Chojnacki <m.chojnacki145@gmail.com>
2023-08-05 13:58:47 +0200

    PlanDao + zmiana w DbUtil (w tym pom.xml)

---
+++ src/main/java/pl/coderslab/dao/PlanDao.java
@@ -0,0 +1,92 @@
+package pl.coderslab.dao;
+
+import pl.coderslab.model.Plan;
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlanDao {
+    private static final String CREATE_PLAN_QUERY = "INSERT INTO plan(name,description,created, admin_id) VALUES (?,?,?,?);";
+    private static final String DELETE_PLAN_QUERY = "DELETE FROM plan where id = ?;";
+    private static final String FIND_ALL_PLANS_QUERY = "SELECT * FROM plan;";
+    private static final String READ_PLAN_QUERY = "SELECT * from plan where id = ?;";
+    private static final String UPDATE_PLAN_QUERY = "UPDATE	plan SET name = ? , description = ?, created = ? WHERE	id = ?;";
+
+    public static void createNewPlan (Plan plan) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement preStmt = connection.prepareStatement(CREATE_PLAN_QUERY);
+            preStmt.setString(1, plan.getName());
+            preStmt.setString(2, plan.getDescription());
+            preStmt.setString(3, plan.getCreated());
+            preStmt.setInt(4, plan.getAdminId());
+            preStmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Plan read(Integer planId) {
+        Plan plan = new Plan();
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(READ_PLAN_QUERY)
+        ) {
+            statement.setInt(1, planId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    plan.setId(resultSet.getInt("id"));
+                    plan.setName(resultSet.getString("name"));
+                    plan.setDescription(resultSet.getString("description"));
+                    plan.setCreated(resultSet.getString("created"));
+                    plan.setAdminId(resultSet.getInt("admin_id"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return plan;
+    }
+    public static List<Plan> findAllPlans () {
+        List<Plan> listOfAllPlans = new ArrayList<>();
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement stmt = connection.prepareStatement(FIND_ALL_PLANS_QUERY);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Plan plan = new Plan();
+                plan.setId(rs.getInt("id"));
+                plan.setName(rs.getString("name"));
+                plan.setDescription(rs.getString("description"));
+                plan.setCreated(rs.getString("created"));
+                plan.setAdminId(rs.getInt("admin_id"));
+                listOfAllPlans.add(plan);
+            }
+            return listOfAllPlans;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void updatePlan (Plan plan) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement preStmt = connection.prepareStatement(UPDATE_PLAN_QUERY);
+            preStmt.setString(1, plan.getName());
+            preStmt.setString(2, plan.getDescription());
+            preStmt.setString(3, plan.getCreated());
+            preStmt.setInt(4, plan.getId());
+            preStmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deletePlan (int planId) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement preStmt = connection.prepareStatement(DELETE_PLAN_QUERY);
+            preStmt.setInt(1, planId);
+            preStmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
