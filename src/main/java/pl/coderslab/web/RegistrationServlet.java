f6cc630d64e334560ee3f7653ed9b20c521f2aa7
Asia Skawinska <joanna.o.skawinska@gmail.com>
2023-08-11 21:11:27 +0200

    Registration - including registration form and handling it, changes in AdminDao

--- src/main/java/pl/coderslab/web/RegistrationServlet.java
+++ src/main/java/pl/coderslab/web/RegistrationServlet.java
@@ -1,23 +1,23 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.model.Admin;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/register")
 public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/register").forward(req, resp);
+        req.getRequestDispatcher("/registration.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         String name = req.getParameter("name");
         String surname = req.getParameter("surname");
         String email = req.getParameter("email");
@@ -25,6 +25,24 @@ public class RegistrationServlet extends HttpServlet {
         String repassword = req.getParameter("repassword");
 
 
-        resp.sendRedirect(""); //strona z info o prawidłowym zarejestrowaniu
+        if (!password.equals(repassword)) {
+
+            req.setAttribute("error", "Hasła nie są takie same");
+            req.getRequestDispatcher("/registration.jsp").forward(req, resp);
+            return;
+        }
+
+
+        Admin admin = new Admin();
+        admin.setFirstName(name);
+        admin.setLastName(surname);
+        admin.setEmail(email);
+        admin.setPassword(password);
+
+
+        pl.coderslab.dao.AdminDao.createNewAdmin(admin);
+
+
+        resp.sendRedirect(req.getContextPath() + "/login.jsp");
     }
 }
