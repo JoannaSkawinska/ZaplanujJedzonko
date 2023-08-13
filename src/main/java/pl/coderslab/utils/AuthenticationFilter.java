615b7c0513bf1e9ec96e317491522ef6bfb7bdbf
Asia Skawinska <joanna.o.skawinska@gmail.com>
2023-08-13 16:27:36 +0200

    Filter class added

---
+++ src/main/java/pl/coderslab/utils/AuthenticationFilter.java
@@ -0,0 +1,40 @@
+package pl.coderslab.utils;
+
+import pl.coderslab.model.Admin;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/app/*")
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        Admin authenticatedAdmin = (Admin) req.getSession().getAttribute("authenticatedAdmin");
+
+        if (authenticatedAdmin != null) {
+
+            chain.doFilter(request, response);
+        } else {
+
+            resp.sendRedirect(req.getContextPath() + "/login");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
