b9a5e261cfdd4a5faa798e7d5166142e1451d563
Asia Skawinska <joanna.o.skawinska@gmail.com>
2023-08-17 21:11:16 +0200

    poprawione dodawanie posiłku do planu uzytkownika

--- src/main/java/pl/coderslab/web/AddRecipeToPlanServlet.java
+++ src/main/java/pl/coderslab/web/AddRecipeToPlanServlet.java
@@ -1,8 +1,12 @@
 package pl.coderslab.web;
+import pl.coderslab.dao.AdminDao;
 import pl.coderslab.dao.DayNameDao;
 import pl.coderslab.dao.PlanDao;
 import pl.coderslab.dao.RecipeDao;
 import pl.coderslab.model.Admin;
+import pl.coderslab.model.DayName;
+import pl.coderslab.model.Plan;
+import pl.coderslab.model.Recipe;
 
 
 import javax.servlet.ServletException;
@@ -12,37 +16,56 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
-        @WebServlet("/app/recipe/plan/add")
-        public class AddRecipeToPlanServlet extends HttpServlet {
-            protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                HttpSession sess = request.getSession();
-                Admin loggedAdmin = (Admin) sess.getAttribute("authenticatedAdmin");
+@WebServlet("/app/recipe/plan/add")
+public class AddRecipeToPlanServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession sess = request.getSession();
+        AdminDao adminDao = new AdminDao();
+        Admin loggedAdmin = adminDao.read(1);
 
-            //    request.setAttribute("plans", PlanDao.findAllPlansOfAdmin());
-                request.setAttribute("recipes", RecipeDao.findAllRecipesOfAdmin(loggedAdmin.getId()));
-                request.setAttribute("days", DayNameDao.findAll());
-                 request.getRequestDispatcher("/app-schedules-meal-recipe.jsp").forward(request, response);
-            }
+        //Admin loggedAdmin = (Admin) sess.getAttribute("authenticatedAdmin");
 
-            protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        sess.setAttribute("authenticatedAdmin", loggedAdmin);
+        List<Plan> planOptions = PlanDao.findAllPlansOfAdmin(loggedAdmin.getId());
+        List<Recipe> recipeOptions = RecipeDao.findAllRecipesOfAdmin(loggedAdmin.getId());
+        List<DayName> dayOptions = DayNameDao.findAll();
 
-                int planId = Integer.parseInt(request.getParameter("choosePlan"));
-                String mealName = request.getParameter("mealName");
-                String recipeName = request.getParameter("recipeName");
-                int dayId = Integer.parseInt(request.getParameter("dayName"));
-                int displayOrder = Integer.parseInt(request.getParameter("number"));
+        request.setAttribute("planOptions", planOptions);
+        request.setAttribute("recipeOptions", recipeOptions);
+        request.setAttribute("dayOptions", dayOptions);
 
+        request.getRequestDispatcher("/app-schedules-meal-recipe.jsp").forward(request, response);
+    }
 
-                PlanDao.addRecipeToPlan(planId, mealName, recipeName, dayId, displayOrder);
+    /*protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession sess = request.getSession();
+        Admin loggedAdmin = (Admin) sess.getAttribute("authenticatedAdmin");
 
-                //dayName jest
+List<Plan> planOptions = PlanDao.findAllPlansOfAdmin(loggedAdmin.getId());
+        List<Recipe> recipeOptions = RecipeDao.findAllRecipesOfAdmin(loggedAdmin.getId());
+        List<DayName> dayOptions = DayNameDao.findAll();
 
-                HttpSession session = request.getSession();
-                session.setAttribute("success", "Przepis dodany do planu");
+        request.setAttribute("planOptions", PlanDao.findAllPlansOfAdmin(loggedAdmin.getId()));
+        request.setAttribute("recipeOptions", RecipeDao.findAllRecipesOfAdmin(loggedAdmin.getId()));
+        request.setAttribute("dayOptions", DayNameDao.findAll());
 
-                response.sendRedirect(request.getContextPath() + "/app/recipe/plan/add");
+        request.getRequestDispatcher("/app-schedules-meal-recipe.jsp").forward(request, response);
+    }*/
 
-            }
-        }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int planId = Integer.parseInt(request.getParameter("choosePlan"));
+        String mealName = request.getParameter("mealName");
+        String recipeName = request.getParameter("recipe");
+        String dayId = request.getParameter("day"); //
+        int displayOrder = Integer.parseInt(request.getParameter("number"));
 
+        PlanDao.addRecipeToPlan(planId, mealName, recipeName, dayId, displayOrder);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("success", "Przepis dodany do planu");
+
+        response.sendRedirect(request.getContextPath() + "/app/recipe/plan/add");
+    }
+}
